7be95729322587df2925e7409ba4eb51f03e5ac4
Carlos_Jorge <c.cuello@estudiantes.matcom.uh.cu>
2019-06-11 17:32:42 -0400

    cambie la administracion de los errores para que si hay un error en el parser se para el programa

--- CmpProject/Program.cs
+++ CmpProject/Program.cs
@@ -17,7 +17,7 @@ namespace CmpProject
         {
             //var streamReader = new StreamReader("Testing/Tipos.cl");
             //var streamReader = new StreamReader("Testing/Dispatch.cl");
-            var streamReader = new StreamReader("Testing/ZAHUIS.cl");
+            //var streamReader = new StreamReader("Testing/ZAHUIS.cl");
             //var streamReader = new StreamReader("Testing/Expresion Case.cool");
             //var streamReader = new StreamReader("Testing/Bool.cool");
             //var streamReader = new StreamReader("Testing/Nuevo tipo.cl");
@@ -26,44 +26,75 @@ namespace CmpProject
             //var streamReader = new StreamReader("Testing/Expresion let.cl");
             //var streamReader = new StreamReader("Testing/Factorial.cl");
             //var streamReader = new StreamReader("Testing/Tipos.cl");
+            var streamReader = new StreamReader("Testing/Example.cl");
+
+            #region Lexer
             var MyLexer = new COOLgrammarLexer(new AntlrInputStream(streamReader.ReadToEnd()));
+            MyLexer.RemoveErrorListeners();
+            var myErrorsLexer = new MyErrorListenerLexer();
+            MyLexer.AddErrorListener(myErrorsLexer);
             streamReader.Close();
+            if (myErrorsLexer.errorLoggers.msgs.Count != 0)
+            {
+                foreach (var e in myErrorsLexer.errorLoggers.msgs)
+                    Console.WriteLine(e);
+                return;
+            }
+            #endregion
+
             var tokens = new CommonTokenStream(MyLexer);
+
+            #region Parser
             var MyParser = new COOLgrammarParser(tokens);
+            MyParser.RemoveErrorListeners();
+            var myErrors = new MyErrorListener();
+            MyParser.AddErrorListener(myErrors);
             var program = MyParser.program();
+            //Si hay errores para la ejecucion del programa
+            if (myErrors.errorLoggers.msgs.Count!=0)
+            {
+                foreach (var e in myErrors.errorLoggers.msgs)
+                    Console.WriteLine(e);
+                return;
+            }
             Console.WriteLine(program.ToStringTree(MyParser));
+            #endregion
+            #region CheckSemantic
             var typeVisitor = new TypeBuilderVisitor();
             typeVisitor.Visit(program);
             var FeatureVisitor = new FeatureBuilderVisitor(typeVisitor);
             FeatureVisitor.Visit(program);
             var CheckSemantic = new CheckSemanticVisitor(FeatureVisitor);
             CheckSemantic.Visit(program);
-            if (CheckSemantic.errorLogger.msgs.Count == 0)
+            if (CheckSemantic.errorLogger.msgs.Count != 0)
             {
-                var generateCilTypes = new GenerateToCilTypes(CheckSemantic);
-                generateCilTypes.Visit(program);
-                var generateCilFeatures = new GenerateToCilFeatures(generateCilTypes);
-                generateCilFeatures.Visit(program);
-                var generateCil = new GenerateToCil(CheckSemantic, generateCilFeatures);
-                //var generateCil = new GenerateToCilOptimization(CheckSemantic, generateCilFeatures);
-                generateCil.Visit(program);
-                Console.WriteLine(generateCil.CilAst);
-                var generateMips = new CilToMips(generateCil);
-                var mips = generateMips.Visit(program);
-                Console.WriteLine(".data");
-                mips.Data.ForEach(x => Console.WriteLine(x));
-                Console.WriteLine(".text");
-                mips.Text.ForEach(x => Console.WriteLine(x));
-                Console.WriteLine("-----functions-----");
-                mips.Functions.ForEach(x => Console.WriteLine(x));
-
+                foreach (var item in CheckSemantic.errorLogger.msgs)
+                    Console.WriteLine(item);
+                return;
             }
-            foreach (var item in CheckSemantic.errorLogger.msgs)
-            {
-                Console.WriteLine(item);
-            }
+            #endregion
+            #region CoolToCil
+            var generateCilTypes = new GenerateToCilTypes(CheckSemantic);
+            generateCilTypes.Visit(program);
+            var generateCilFeatures = new GenerateToCilFeatures(generateCilTypes);
+            generateCilFeatures.Visit(program);
+            var generateCil = new GenerateToCil(CheckSemantic, generateCilFeatures);
+            //var generateCil = new GenerateToCilOptimization(CheckSemantic, generateCilFeatures);
+            generateCil.Visit(program);
+            Console.WriteLine(generateCil.CilAst);
+            #endregion
+            #region CilToMips
+            var generateMips = new CilToMips(generateCil);
+            var mips = generateMips.Visit(program);
+            Console.WriteLine(".data");
+            mips.Data.ForEach(x => Console.WriteLine(x));
+            Console.WriteLine(".text");
+            mips.Text.ForEach(x => Console.WriteLine(x));
+            Console.WriteLine("-----functions-----");
+            mips.Functions.ForEach(x => Console.WriteLine(x));
+            #endregion
         }
 
-       
+
     }
 }
